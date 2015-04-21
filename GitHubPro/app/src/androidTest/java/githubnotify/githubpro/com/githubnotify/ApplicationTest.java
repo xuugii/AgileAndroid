f8065499b060efa4d0a4c159521b3ba904429185
Uguudei <uguudei@gmail.com>
2015-04-21 15:48:17 +0200

    Init Android APP

---
+++ GitHubPro/app/src/androidTest/java/githubnotify/githubpro/com/githubnotify/ApplicationTest.java
@@ -0,0 +1,13 @@
+package githubnotify.githubpro.com.githubnotify;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
