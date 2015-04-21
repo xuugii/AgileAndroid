74e91a782f409a9aeca15d286e3b1a483b216dc8
Uguudei <uguudei@gmail.com>
2015-04-21 16:41:21 +0200

    Update For the java test APP

--- InternalLib/Test/src/MainStart.java
+++ InternalLib/Test/src/MainStart.java
@@ -12,13 +12,10 @@ public class MainStart {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GitHubClient client = new GitHubClient();
-		client.setOAuth2Token("76bfa8a2dbc64d20ba05d65f85261a1da51f4915");
+		client.setOAuth2Token("242613366a410ff2d7c160bf199bcf3cce3e6899");
 		RepositoryService service = new RepositoryService(client);
 		CommitService commitService = new CommitService(client);
 		GitHubRequest tmp = new GitHubRequest();
-		
-		
-		
 		try {
 			for (Repository repo : service.getRepositories("xuugii")){
 				
